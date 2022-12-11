a4660fc23580148aae9dc839f01e8d6f5e267081
Arnold <iarnoldraphael@gmail.com>
2022-12-11 14:56:13 +0300

    added create new objects

---
+++ lms.js
@@ -0,0 +1,28 @@
+class Role {
+    constructor(userName, email) {
+        this.userName = userName
+        this.email = email
+    }
+
+    uploadVideo() {
+        console.log(`Hey ${this.userName}!, upload some new videos`)
+    }
+
+    viewVideo() {
+        console.log(`Hey ${this.userName}!, new videos release here`)
+    }
+
+    static loggedIn() {
+        console.log('This user is logged in')
+    }
+}
+
+const tutor = new Role('MrLevinius', 'example@mail.com')
+tutor.uploadVideo()
+Role.loggedIn()
+
+console.log('\n\n')
+
+const student = new Role('Arnold', 'iarnold@gmail.com')
+student.viewVideo()
+Role.loggedIn()
